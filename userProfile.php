ffe2f1412029214dc483849630ee81131188be16
Nahid940 <nahid940@gmail.com>
2017-09-01 23:47:32 +0600

    Modal initialized on userprofile page

--- userProfile.php
+++ userProfile.php
@@ -27,18 +27,25 @@ if(isset($_GET['action']) && $_GET['action']=='logout')
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- Custom styles for this template -->
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link href="css/heroic-features.css" rel="stylesheet">
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
   </head>
 
   <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  
+
+    <!-- Page Content -->
+    <div class="container">
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
         <a class="navbar-brand" href="#">Start Bootstrap</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -67,21 +74,10 @@ if(isset($_GET['action']) && $_GET['action']=='logout')
         </div>
       </div>
     </nav>
-
-    <!-- Page Content -->
-    <div class="container">
-
-      <!-- Jumbotron Header -->
-<!--
-      <header class="jumbotron my-4">
-      
-        <h1 class="display-3">A Warm Welcome! <?php  echo $name;?></h1>
-        <p class="lead"></p>
-        <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
-      </header>
--->
-
-      <!-- Page Features -->
+     
+     
+     
+     
       <div class="row text-center">
                  <?php
                 foreach($user->getUserInformation() as $userData){             
@@ -125,65 +121,62 @@ if(isset($_GET['action']) && $_GET['action']=='logout')
                       <tr>
                         <td>Date of Birth</td>
                         <td><?php echo $userData['dob']?></td>
+                        <td>
+                        <form action="" method="post">
+<!--                             <input type="button" data-toggle="modal" data-target="#view-modal" class="btn btn-info editevent" value="Edit">-->
+                       <input type="button" data-toggle="modal" data-target="#view-modal" value="Update" name="edit"  class="btn btn-primary editevent" data-id="15" id="editevent" />
+                       
+                       
+                        </form>
+                       </td>
                       </tr>
                     </tbody>
                   </table>
-                       
                         </div>
                     </div>
                 </div>
-            </div>
-         </div>        
-               <?php }?> 
-              
-             
                 
-<!--            </div>-->
-<!--        </div>-->
-
-<!--
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
             </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 mb-4">
-          <div class="card">
-            <img class="card-img-top" src="http://placehold.it/500x325" alt="">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Find Out More!</a>
-            </div>
-          </div>
-        </div>
--->
+             
+         </div>        
+    <?php }?> 
+              
 
       </div>
       <!-- /.row -->
+      
+              <div id="view-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                  <div class="modal-dialog"> 
+                     <div class="modal-content"> 
+                        <div class="modal-header"> 
+                           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> 
+                           <h4 class="modal-title">
+                               <i class="fa fa-pencil" aria-hidden="true"></i> Event details
+                           </h4> 
+                        </div> 
+
+                        <div class="modal-body">                     
+                           <div id="modal-loader" style="display: none; text-align: center;">
+                           <!-- ajax loader -->
+                           <img src="ajax-loader.gif">
+                           </div>
+
+                           <!-- mysql data will be load here -->                          
+<!--                           <div id="dynamic-content"></div>-->
+                        </div> 
+
+                        <div class="modal-footer"> 
+                            <button type="button" class="btn btn-primary" onclick="UpdateeventDetails()">Update</button>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>  
+                        </div> 
+
+                    </div> 
+                  </div>
+            </div>
+      
+      
+      
+      
 
     </div>
     <!-- /.container -->
@@ -197,9 +190,45 @@ if(isset($_GET['action']) && $_GET['action']=='logout')
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    
 <!--    <script src="vendor/popper/popper.min.js"></script>-->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>-->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+   
+    
+    
+       <script>
+        $(document).ready(function() {
+        $(".editevent").on('click', function(e) {
+        // Get the record's ID via attribute
+            e.preventDefault();
+            var id = $(this).attr('data-id');
+            $('#dynamic-content').html('');
+            $('#modal-loader').show();
+            $('#modal-loader').hide();
+            alert("ok");
+            
+//             $.ajax({
+//              url: 'editevent.php',
+//              type: 'POST',
+//              data: {
+//                  id:id,
+//              },
+//              dataType: 'html'
+//         }).done(function(data){
+//              console.log(data); 
+//              $('#dynamic-content').html(''); // blank before load.
+//              $('#dynamic-content').html(data); // load here
+//              $('#modal-loader').hide(); // hide loader  
+//         }).fail(function(){
+//          $('#dynamic-content').html('<i class="glyphicon glyphicon-info-sign"></i> Something went wrong, Please try again...');
+//          $('#modal-loader').hide();
+//     });
+            
+        });
+    });
+    </script>
 
   </body>
 
